d736366727c138cafbc85891ac76aa65adf4225b
root <root@localhost.localdomain>
2015-12-08 21:38:57 +0800

    examples

--- examples/http.lua
+++ examples/http.lua
@@ -4,6 +4,7 @@ local http = require "http"
 local tbl = require "tbl"
 
 shaco.start(function()
+    print ('get ...')
     local code, body = http.get("www.baidu.com", "/")
     print (code)
     print(body)
--- examples/webserver.lua
+++ examples/webserver.lua
@@ -5,28 +5,23 @@ local socket = require "socket"
 
 shaco.start(function()
     local root = "../html/"
-    local host = shaco.getenv("host") or "0.0.0.0:1234"
-    local ip, port = host:match("([^:]+):?([%d]+)$")
-    print (ip, port)
-    local lid = assert(socket.listen(ip, port))
-    print ("listen on",lid,host)
-    socket.start(lid, function(id)
-        print ("accept", id)
-        shaco.fork(function()
+    local addr = '127.0.0.1:1234'
+    print ("listen on "..addr)
+    local sock = assert(socket.listen(
+        addr,
+        function(id)
+            print ("accept", id)
             socket.start(id)
-            socket.readenable(id, true)
+            socket.readon(id)
             local code, method, uri, head_t, body = http.read(id)
-            print(code, method, uri, head_t, body)
-            tbl.print(head_t)
+            print ('code:'..code, 'method:'..method, 'uri:'..uri)
+            tbl.print(head_t, '[head]')
+            print('[body]='..body)
             code = 200
             body = "1234"
-            local f = io.open(root.."index.html")
-            local s = f:read("*a")
-            body = s
             head_t = {}
             head_t["content-type"] = "text/html; charset=utf8"
             http.response(id, code, body, head_t)
             socket.shutdown(id)
-        end)  
-    end)
+        end))
 end)
